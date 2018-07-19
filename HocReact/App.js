ae7304c44039d058019c065e94a6af26cc589f8c
lequochuy <lequochuysv@gmail.com>
2018-07-19 11:21:50 +0700

    text

--- HocReact/App.js
+++ HocReact/App.js
@@ -13,13 +13,18 @@ import {
 } from 'react-native';
 
 export default class App extends Component<Props> {
+  _onPress(event) {
+    alert(event.nativeEvent);
+  }
+
+  _onLongPress(event) {
+    alert('Long click');
+  }
   render() {
     return (
       <View style={{flex: 1, flexDirection: 'row'}}>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
-      </View>
+        <Text onLongPress = { this._onLongPress.bind(this) } onPress = {this._onPress.bind(this)} ellipsizeMode = {'tail'} numberOfLines = {1} selectable = { true }>Thà để ta phụ người khác chứ không để người khác phụ ta.Thà để ta phụ người khác chứ không để người khác phụ ta.</Text>
+      </View>  
     );
   }
 }
